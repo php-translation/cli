8844f01ad978a03285eccf34422218c3414500fd
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-24 10:51:25 +0200

    Have the latest version

--- bump-version.sh
+++ bump-version.sh
@@ -53,9 +53,14 @@ make build
 #
 git checkout gh-pages
 
+cp build/translation.phar downloads/translation.phar
+sha1sum downloads/translation.phar > downloads/translation.version
+git add downloads/translation.phar
+git add downloads/translation.version
+
 cp build/translation.phar downloads/translation-${TAG}.phar
-git add downloads/translation-${TAG}.phar
 sha1sum downloads/translation-${TAG}.phar > downloads/translation-${TAG}.version
+git add downloads/translation-${TAG}.phar
 git add downloads/translation-${TAG}.version
 
 SHA1=$(openssl sha1 build/translation.phar)
